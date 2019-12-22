05deb2a52fb1cccb1aa4c999718216f75d560374
liukai <liukai@yzyw-inc.com>
2019-12-22 23:24:50 +0800

    ADD: 原子变量与非阻塞同步机制

--- src/main/java/com/liukai/concurrent/bingfabianchengshizhan/ch15/PseudoRandom.java
+++ src/main/java/com/liukai/concurrent/bingfabianchengshizhan/ch15/PseudoRandom.java
@@ -1,6 +1,7 @@
 package com.liukai.concurrent.bingfabianchengshizhan.ch15;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 假冒的随机数
@@ -12,18 +13,37 @@ public abstract class PseudoRandom {
   public static void main(String[] args) {
     // 重入锁版本的伪随机数生成器
     PseudoRandom pseudoRandom = new ReentrantLockPseudoRandom(10);
-    int nThread = 10;
-    for (int i = 0; i < nThread; i++) {
+    // 原子变量获取伪随机数生成器
+    // PseudoRandom pseudoRandom = new AtomicPseudoRandom(10);
+    // 使用线程本地化伪随机数生成器
+    // PseudoRandom pseudoRandom = new ThreadLocalPseudoRandom();
+    long start = System.currentTimeMillis();
+    int nThread = 50;
+
+    for (int i = 1; i < nThread; i++) {
       int finalI = i;
       new Thread(() -> {
         int next = pseudoRandom.calculateNext(finalI);
-
+        System.out.println(Thread.currentThread().getName() + "\t获取随机数：" + next);
       }, "线程" + i).start();
     }
 
+    // 检测上述线程生命周期已经结束
+    while (Thread.activeCount() > 1) {
+      // do nothing
+    }
+    System.out.println();
+    System.out.println(
+      nThread + "个线程使用\t" + pseudoRandom.getClass().getSimpleName() + "\t随机数生成器一共耗时：" + (
+        System.currentTimeMillis() - start) + "ms");
   }
 
   protected int calculateNext(int s) {
+    try {
+      TimeUnit.NANOSECONDS.sleep(10);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     return random.nextInt(s);
   }
 
